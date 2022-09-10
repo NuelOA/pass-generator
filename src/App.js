890aae54e1abaa4590f1911b286601a23c881a37
Nana-Kwame Okyere Asare <nana-kwameokyereasare@Nana-Kwames-MacBook-Pro.local>
2022-09-10 17:17:41 +0000

    pass-gev1.0

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
+import logo from './key.ico';
 import './App.css';
+
+
+const gen = () =>{
+ let genPass = document.getElementById('passowrd')
+ let randomstring = Math.random().toString(36).slice(-8);
+ 
+ genPass.innerHTML=randomstring
+}
+
+ async function copyPass(){
+    let genPass = document.getElementById('passowrd')
+    let content = genPass.textContent
+    try{
+      await navigator.clipboard.writeText(content);
+      alert("copied")
+    }catch(error){
+       console.log(error)
+    }
+
+ }
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src={logo} className="App-logo" alt="logo" id='image' />
+        <h3>Click On Button To Generate <br/>Password 😎 🔒</h3>
+         <p id='passowrd'></p>
+
       </header>
+
+      <button id='btn' onClick={gen}>Generate </button> &nbsp;  <button id='btn-copy' onClick={copyPass}>Copy </button>   
     </div>
   );
 }
