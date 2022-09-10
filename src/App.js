cbe545de5355afb9acbe6eb9d0fede288428234c
Nana-Kwame Okyere Asare <nana-kwameokyereasare@Nana-Kwames-MacBook-Pro.local>
2022-09-10 18:52:00 +0000

    v1.0 update on 10 characters

--- src/App.js
+++ src/App.js
@@ -2,12 +2,22 @@ import logo from './key.ico';
 import './App.css';
 
 
-const gen = () =>{
- let genPass = document.getElementById('passowrd')
- let randomstring = Math.random().toString(36).slice(-8);
+const checked = () =>{
+ let check = document.getElementById('check');
+ if(check.checked === true){
+  let genPass = document.getElementById('passowrd')
+ let randomstring = Math.random().toString(36).slice(-10);
  
  genPass.innerHTML=randomstring
+ }else{
+  let genPass = document.getElementById('passowrd')
+  let randomstring = Math.random().toString(36).slice(-8);
+  
+  genPass.innerHTML=randomstring
+ }
+ 
 }
+
 
  async function copyPass(){
     let genPass = document.getElementById('passowrd')
@@ -22,17 +32,20 @@ const gen = () =>{
  }
 
 
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" id='image' />
         <h3>Click On Button To Generate <br/>Password 😎 🔒</h3>
+        <small className='check'><b>8 characters default</b></small>
+        <small className='check'>Check for 10 characters <input type="checkbox" id='check'onClick={checked} ></input></small>
          <p id='passowrd'></p>
 
       </header>
-
-      <button id='btn' onClick={gen}>Generate </button> &nbsp;  <button id='btn-copy' onClick={copyPass}>Copy </button>   
+      <button id='btn' onClick={checked}>Generate </button> &nbsp;  <button id='btn-copy' onClick={copyPass}>Copy </button>  
+       
     </div>
   );
 }
